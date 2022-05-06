43016bc6045445b8c28d75c8bc1482c679c1976b
zunuu <zunayedhossain99@gmail.com>
2022-05-07 02:11:13 +0600

    data for mongodb

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const cors = require('cors');
 const port = process.env.port || 5000;
 require('dotenv').config();
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('spices den server')
+})
+// listening //
+app.listen(port, () => {
+    console.log('hi i am listening');
+})
