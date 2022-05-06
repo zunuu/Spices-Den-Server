8278073f3a3732d6fd290abdc9094b4748e91ba2
zunuu <zunayedhossain99@gmail.com>
2022-05-07 04:20:24 +0600

    heroku-added

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+/////////////// connection string //////////////////
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.avnz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        //client connect
+        await client.connect();
+        const inventoryCollection = client.db('spices-den-db').collection('inventory-items');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('spices den server')
 })
